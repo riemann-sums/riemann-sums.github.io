c0154e246e13406f0d40f345242c2334a05a605d
Preston Crowe <prestonjcrowe@gmail.com>
2017-09-12 00:39:49 -0700

    Math.js dependency

--- sketch.js
+++ sketch.js
@@ -8,32 +8,34 @@
 // -[ ] comments on parsing code, credit to knexcar
 // -[x] could use noloop(), only redraw on slider change?
 // -[ ] add buttons for changing tick #, type, n, and function
-// -[ ] rewrite hastily crafted button class (pls)
-// -[ ] readme.md
+// -[ ] implement own text entry / dialog for f(x) prompt
 // -[ ] separate grid lines and labels (lines behind, labels in front)
+// -[ ] rewrite hastily crafted button class (pls), dolan suggestions
+// -[ ] readme.md
+
 
 var graph,
     sums,
     testPoints = [],
-    mouseHeld = false;
+    mouseHeld = false,
+    activeFunction = math.eval("f(x) = " + "0.2 * sin(x) + 0.1 * x");
+    // do even need this var? mhm
 
 const ACCURACY = 10000;         // number of points computed
-const MAX_N = 75;               // max value of the n slider
+const MAX_N = 1000;               // max value of the n slider
 const INIT_MAX_X = 2 * Math.PI; // initial graph domain (both min and max)
 
 function setup() {	
     createCanvas(windowWidth, windowHeight);
     background(27, 29, 28);
+    console.log("f(x): " + activeFunction(2));
 
-    var x;
-    for (var i = 0; i < ACCURACY; i++) {
-        x = -INIT_MAX_X + (2 * INIT_MAX_X) * (i / ACCURACY);
-        testPoints.push(0.2 * sin(x) + 0.1 * x);
-    }
+    populatePoints();
     graph = new Graph(testPoints, 0, INIT_MAX_X, INIT_MAX_X, 1, 1); 
 }
 
 function draw() {
+    //populatePoints();
     noLoop();
     noSmooth();
     background(27, 29, 28);
@@ -41,6 +43,14 @@ function draw() {
     graph.drawAxes(20);
     graph.drawTopBar();
     graph.drawActiveSums();
+}
+
+function populatePoints() {
+    var x;
+    for (var i = 0; i < ACCURACY; i++) {
+        x = -INIT_MAX_X + (2 * INIT_MAX_X) * (i / ACCURACY);
+        testPoints.push(activeFunction(x));
+    }
 }
 
 function windowResized() {
@@ -72,4 +82,12 @@ function mouseDragged() {
         graph.setN(Math.round(graph.slider.getPortion() * MAX_N));
         redraw();
     }
+}
+
+function keyPressed() {
+    var input ="f(x) = " +  prompt("Enter new f(x): ");
+    // need to validate here, regex?
+    activeFunction = math.eval(input);
+    populatePoints();
+
 }
